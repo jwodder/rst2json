ce09d37060adb7dbdf4d5b459c18373f4c0823ee
John T. Wodder II <git@varonathe.org>
2020-07-05 22:48:05 -0400

    Packaging boilerplate

---
+++ src/rst2json/__init__.py
@@ -0,0 +1,11 @@
+"""
+Convert reStructuredText to broken-out JSON
+
+Visit <https://github.com/jwodder/rst2json> for more information.
+"""
+
+__version__      = '0.1.0.dev1'
+__author__       = 'John Thorvald Wodder II'
+__author_email__ = 'rst2json@varonathe.org'
+__license__      = 'MIT'
+__url__          = 'https://github.com/jwodder/rst2json'
