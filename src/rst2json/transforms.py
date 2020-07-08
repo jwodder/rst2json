6ab6a7fd3d5b56c86d2cc9036fc46e3e793b6ba3
John T. Wodder II <git@varonathe.org>
2020-07-08 13:19:08 -0400

    Adjust custom transform

--- src/rst2json/transforms.py
+++ src/rst2json/transforms.py
@@ -3,21 +3,22 @@ from docutils.transforms import Transform
 
 class MoveEmbeddedSystemMessages(Transform):
     """
-    Move all "embedded" system messages (ones neither directly below the
-    document nor in a "system-messages" section) to the end of the document.
-    This keeps them from causing problems when rendering nodes containing
-    system messages.
+    Before this transform runs, some of the system messages in the document are
+    "embedded" in the document (``node.parent is not None``) and some are not;
+    the ``Messages`` transform moves the latter group to a special system
+    messages section.  This transform detaches the former group from the
+    document so that they will not cause problems when rendering the nodes that
+    (used to) contain them.
+
+    (Note: I'm assuming here that the only system messages that ever end up
+    "embedded" in a document are generated while processing transforms and thus
+    are listed in in ``document.transform_messages``.)
     """
 
-    default_priority = 875
+    default_priority = 855
 
     def apply(self):
-        messages = []
         for node in tuple(self.document.traverse(nodes.system_message)):
-            if not isinstance(node.parent, nodes.document) and not (
-                isinstance(node.parent, nodes.section)
-                and 'system-messages' in node.parent["classes"]
-            ):
-                messages.append(node)
+            if node.parent is not None:
                 node.parent.remove(node)
-        self.document += messages
+                node.parent = None
