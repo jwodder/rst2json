83d11d0cb3e4506882ef5e0064048f4e1d04ab45
John T. Wodder II <git@varonathe.org>
2020-07-08 13:45:57 -0400

    Adjust again

--- src/rst2json/transforms.py
+++ src/rst2json/transforms.py
@@ -3,22 +3,22 @@ from docutils.transforms import Transform
 
 class MoveEmbeddedSystemMessages(Transform):
     """
-    Before this transform runs, some of the system messages in the document are
-    "embedded" in the document (``node.parent is not None``) and some are not;
-    the ``Messages`` transform moves the latter group to a special system
-    messages section.  This transform detaches the former group from the
-    document so that they will not cause problems when rendering the nodes that
-    (used to) contain them.
-
-    (Note: I'm assuming here that the only system messages that ever end up
-    "embedded" in a document are generated while processing transforms and thus
-    are listed in in ``document.transform_messages``.)
+    In order to keep "embedded" system messages (ones actually added to the
+    document; note that this transform precedes ``Messages``, which adds
+    "stragglers" to a special section at the end of the document) from causing
+    problems when rendering their parent nodes, this transform detaches all
+    such messages from the document and then moves them to the end.  Messages
+    listed in ``document.transform_messages`` are detached but not relocated,
+    leaving the latter step to the ``Messages`` transform.
     """
 
     default_priority = 855
 
     def apply(self):
+        messages = []
         for node in tuple(self.document.traverse(nodes.system_message)):
-            if node.parent is not None:
-                node.parent.remove(node)
-                node.parent = None
+            node.parent.remove(node)
+            node.parent = None
+            if node not in self.document.transform_messages:
+                messages.append(node)
+        self.document += messages
