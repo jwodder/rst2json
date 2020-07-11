07b0f4fc77dbba411ab28e8f4b30b3419fbb0420
John T. Wodder II <git@varonathe.org>
2020-07-11 22:31:46 +0000

    v0.2.0 — Convenience function
    - Added a convenience function, ``rst2json()``, for converting reStructuredText to JSON directly within Python
    - Added information to the README on using the library's writer classes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
-v0.2.0 (in development)
------------------------
+v0.2.0 (2020-07-11)
+-------------------
 - Added a convenience function, ``rst2json()``, for converting reStructuredText
   to JSON directly within Python
 - Added information to the README on using the library's writer classes
--- src/rst2json/__init__.py
+++ src/rst2json/__init__.py
@@ -22,7 +22,7 @@ Sample templates that make use of the library's output can be found in
 Visit <https://github.com/jwodder/rst2json> for more information.
 """
 
-__version__      = '0.2.0.dev1'
+__version__      = '0.2.0'
 __author__       = 'John Thorvald Wodder II'
 __author_email__ = 'rst2json@varonathe.org'
 __license__      = 'MIT'
